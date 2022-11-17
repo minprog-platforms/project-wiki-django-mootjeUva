999367a582764ce5270a4564a1a0cb9a80d30c17
Mohammed Wafelgha <mohammedwafelgha@hotmail.com>
2022-11-17 16:33:49 +0100

    change8

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,36 +7,54 @@ from django.urls import reverse
 from markdown2 import Markdown as markdown
 from . import util
 import random
+from django import forms
+
+
+class SearchForm(forms.Form):
+    """ Form Class for Search Bar """
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+      "class": "search",
+      "placeholder": "Search Encyclopedia"}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": SearchForm()
     })
 
 def entry(request, title):
     wiki_entry = markdown().convert(util.get_entry(title))
     return render(request, "encyclopedia/entry.html", {
         "title": title,
-        "entry": wiki_entry
+        "entry": wiki_entry,
+        "search_form": SearchForm(),
     })
 
 def editpage(request, title):
     wiki_entry = markdown().convert(util.get_entry(title))
     return render(request, "encyclopedia/editpage.html", {
         "title": title,
-        "entry": wiki_entry
+        "entry": wiki_entry,
+        "search_form": SearchForm()
     })
 
-def search(request, title):
-    wiki_entry = util.get_entry(title)
-    return redirect(reverse('entry', args=[title]))
-    #return render(request, "encyclopedia/search.html", {
-    #    "title": title
-    #})
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            wiki_entry = util.get_entry(title)
+            if wiki_entry:
+                return redirect(reverse('entry', args=[title]))
+            else:
+                return redirect(reverse('index'))
 
 
-def newpage(request, title):
-    pass
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html", {
+          "search_form": SearchForm()
+        })
 
 
 def randompage(request):
