78c068e19be6c427d77ae45939e5a0ecb445e760
Mohammed Wafelgha <mohammedwafelgha@hotmail.com>
2022-11-17 17:21:47 +0100

    change

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,8 +10,7 @@ import random
 from django import forms
 
 
-class SearchForm(forms.Form):
-    """ Form Class for Search Bar """
+class Searchclass(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={
       "class": "search",
       "placeholder": "Search Encyclopedia"}))
@@ -19,7 +18,7 @@ class SearchForm(forms.Form):
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "search_form": SearchForm()
+        "search_class": Searchclass()
     })
 
 def entry(request, title):
@@ -27,7 +26,7 @@ def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title,
         "entry": wiki_entry,
-        "search_form": SearchForm(),
+        "search_class": Searchclass(),
     })
 
 def editpage(request, title):
@@ -35,25 +34,28 @@ def editpage(request, title):
     return render(request, "encyclopedia/editpage.html", {
         "title": title,
         "entry": wiki_entry,
-        "search_form": SearchForm()
+        "search_class": Searchclass()
     })
 
 def search(request):
     if request.method == "POST":
-        form = SearchForm(request.POST)
+        form = Searchclass(request.POST)
         if form.is_valid():
             title = form.cleaned_data["title"]
             wiki_entry = util.get_entry(title)
             if wiki_entry:
                 return redirect(reverse('entry', args=[title]))
             else:
-                return redirect(reverse('index'))
+                return render(request, "encyclopedia/search.html", {
+                    "title": title,
+                    "search_class": Searchclass()
+                })
 
 
 
 def newpage(request):
     return render(request, "encyclopedia/newpage.html", {
-          "search_form": SearchForm()
+          "search_class": Searchclass()
         })
 
 
